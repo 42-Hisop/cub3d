779fd5b0a15838879c37a6b52bb58bc0df9ced7c
Hisop <hisop@Hisopui-MacBookPro.local>
2021-06-16 15:22:49 +0900

    0614

--- map_cut.c
+++ map_cut.c
@@ -1,6 +1,28 @@
 #include "includes/cub3d.h"
 #include "includes/split.h"
 
+int		check_map_w(char **line)
+{
+	int		count;
+	int 	i;
+	int		j;
+
+	count = 0;
+	i = 0;
+	while (line[i])
+	{
+		j = 0;
+		while (line[i][j])
+		{
+			if (count <= j)
+				count = j;
+			j++;
+		}
+		i++;
+	}
+	return (count);
+}
+
 int		check_map_h(char **line)
 {
 	int		i;
@@ -16,23 +38,48 @@ int		check_map_h(char **line)
 	return (count);
 }
 
+char	*dup_map(char *map, char *line, int map_w)
+{
+	int		i;
+
+	i = 0;
+	while (line[i])
+	{
+		map[i] = line[i];
+		i++;
+	}
+	while (i < map_w)
+	{
+		map[i] = ' ';
+		i++;
+	}
+	map[i] = '\0';
+	return (map);
+}
+
 char	**cut_map(char **line)
 {
 	//8부터 사용
+	int		map_w;
+	int		map_h;
 	int		i;
 	int		j;
 	char 	**map;
 
-	if (!(map = (char **)malloc(sizeof(char *) * (check_map_h(line) + 1))))
+	map_w = check_map_w(line);
+	map_h = check_map_h(line);
+	if (!(map = (char **)malloc(sizeof(char *) * (map_h + 1))))
 		return (error());
-	i = 8;
-	j = 0;
-	while (line[i])
+	i = 0;
+	j = 8;
+	while (i <= map_h)
 	{
-		map[j] = ft_strdup(line[i]);
+		if (!(map[i] = (char *)malloc(sizeof(char) * (map_w + 1))))
+			return (error());
+		map[i] = dup_map(map[i], line[j], map_w);
 		i++;
 		j++;
 	}
-	map[j] = NULL;
+	map[i] = NULL;
 	return (map);
 }
--- map_parsing.c
+++ map_parsing.c
@@ -6,9 +6,34 @@
 /*   By: khee-seo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/09 19:14:23 by khee-seo          #+#    #+#             */
-/*   Updated: 2021/06/09 21:55:25 by khee-seo         ###   ########.fr       */
+/*   Updated: 2021/06/16 01:04:42 by khee-seo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+int		wallcheck_w(char **map)
+{
+	int		i;
+	int		j;
+	int		flag;
+
+	i = 0;
+	while (map[i])
+	{
+		flag = 0;
+		j = 0;
+		while (map[i][j])
+		{
+			if ((map[i][j] == '0' || map[i][j] == 'E' || map[i][j] == 'W'
+					|| map[i][j] == 'S' || map[i][j] == 'N') && flag == 0)
+				return (1);
+			if (map[i][j] == '1' && flag == 0)
+				flag = 1;
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
 
 int		validcheck(char **map)
 {
@@ -22,8 +47,9 @@ int		validcheck(char **map)
 		while (map[i][j])
 		{
 			if (!(map[i][j] == '0' || map[i][j] == '1' || map[i][j] == 'E'
-				|| map[i][j] == 'W' || map[i][j] == 'S' || map[i][j] == 'N'))
-					return (1);
+				|| map[i][j] == 'W' || map[i][j] == 'S' || map[i][j] == 'N'
+				|| map[i][j] == ' '))
+				return (1);
 			j++;
 		}
 		i++;
